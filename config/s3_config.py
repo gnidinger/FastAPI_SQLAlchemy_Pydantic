7130e21b76d9e8429a98f87ee871f621d6a9f4f4
gnidinger <gnidinger@gmail.com>
2023-10-12 11:28:09 +0900

    feat:s3_async

--- config/s3_config.py
+++ config/s3_config.py
@@ -1,9 +1,11 @@
-import boto3
+import aioboto3
 from config import settings
 
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=settings.S3_ACCESS_KEY,
-    aws_secret_access_key=settings.S3_SECRET_KEY,
-    region_name="ap-northeast-2",
-)
+
+async def get_s3_client():
+    return aioboto3.client(
+        "s3",
+        aws_access_key_id=settings.S3_ACCESS_KEY,
+        aws_secret_access_key=settings.S3_SECRET_KEY,
+        region_name="ap-northeast-2",
+    )
